2f10196817dd5563b73713adacefc262b4280eb2
Alvin Lim <alim459@gmail.com>
2016-02-28 06:41:24 -0800

    working?

--- fitbit.java
+++
@@ -1,9 +0,0 @@
-String client_id = "227GV4";
-String response_type = "token";
-String scope = "activity";
-URI redirect_uri = "http://www.ubc.ca/";
-
-String httpsString = "https://www.fitbit.com/oauth2/authorize?response_type="+token+"&client_id="+client_id+"&redirect_uri="+redirect_uri+
-
-https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=22942C&redirect_uri=http%3A%2F%2Fexample.com%2Ffitbit_auth&scope=activity%20nutrition%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800
-
--- fitbit.js
+++ fitbit.js
@@ -1,13 +1,19 @@
+var Firebase = require("firebase");
+var myFirebaseRef = new Firebase("https://glowing-heat-1885.firebaseio.com/");
 var userID = "4CQ4C6";
-var refreshToken = "e83a51588cad651691ee4942ec08e702266a012d0512de6e5cce4d6c4e50cd52";
+var refreshToken = "93aa6e837ec39de03c318f28da93d9d4b36219aca447554049a2293568772ee6";
 var encodedDev = "MjI3R1Y0OjNjNmFjMGJjODkxOWU2ZjNhZDExMjI5YmM4YTk5MmMw";
 var today = "2016-02-28";
 var refreshURL = "https://api.fitbit.com/oauth2/token?grant_type=refresh_token&refresh_token=";
-var accessToken = "eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE0NTY2NTYyNjcsInNjb3BlcyI6InJhY3QiLCJzdWIiOiI0Q1E0QzYiLCJhdWQiOiIyMjdHVjQiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJpYXQiOjE0NTY2NTI2Njd9.Kg6THtmzr-_qxUzjw58UIx8lBxlHtQGD6o4tEYBInJI";
+var accessToken = "eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE0NTY2NjM2NDUsInNjb3BlcyI6InJhY3QiLCJzdWIiOiI0Q1E0QzYiLCJhdWQiOiIyMjdHVjQiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJpYXQiOjE0NTY2NjAwNDV9.ILDuUnNqGrHLt0tQ84eKkxLyOhPIedSV9gypbIVEjvc";
 
-function refreshAccessToken(){
+function start(){
+	setInterval(getStepInfo, 2000);
+}
 
-	var URL = refreshURL+refreshToken;
+function refreshAccessToken(refToken){
+
+	var URL = refreshURL+refToken;
  	var request = require('request');
 	request({
 		url: URL,
@@ -16,18 +22,27 @@ function refreshAccessToken(){
 			'Content-Type': 'application/x-www-form-urlencoded'
 		},
 		method: 'POST'
-	}, //the callback function when something is successfully stored in elasticsearch
+	},
 		function(error, response, body){
 			if(error) {
 	    		console.log(error);
 			} else {
 				var tokenJSON = JSON.parse(body);
-				accessToken = tokenJSON.access_token;
-				refreshToken = tokenJSON.refresh_token;
-	    		console.log(refreshToken);
+				setAccessTokenFirebase(tokenJSON.access_token);
+				setRefreshTokenFirebase(tokenJSON.refresh_token);
 		  }
 	   }
     );
+}
+
+function setAccessTokenFirebase(acc){
+	accessToken = acc;
+	return;
+}
+
+function setRefreshTokenFirebase(refresh){
+	refreshToken = refresh;
+	return;
 }
 
 function getStepInfo(){
@@ -56,103 +71,108 @@ function getStepInfo(){
 
 function setToFirebase(steps){
 	var step = steps;
-	var https = require("https"),
-    data = {
-	  	Team_1: {
-		    Alvin: {
-		    	steps: step,
-		    	img: "/images/alvin.jpg",
-		    	Token_info: {
-	  				AccToken: accessToken,
-	  				RefToken: refreshToken
-	  			}
-		    },
-		    Bro: {
-		    	steps: 1,
-		    	img: "/images/bro.jpg",
-		    	Token_info: {
-	  				AccToken: 0,
-	  				RefToken: 0
-	  			}
-		    },
-		    Helen: {
-		    	steps: 1,
-		    	img: "/images/helen.jpg",
-		    	Token_info: {
-	  				AccToken: 0,
-	  				RefToken: 0
-	  			}
-		    },
-		    Paul: {
-		    	steps: 1,
-		    	img: "/images/paul.jpg",
-		    	Token_info: {
-	  				AccToken: 0,
-	  				RefToken: 0
-	  			}
-		    },
-		    Wes: {
-		    	steps: 1,
-		    	img: "/images/wes.jpg",
-		    	Token_info: {
-	  				AccToken: 0,
-	  				RefToken: 0
-	  			}
-		    }
-		},
-		Team_2: {
-		    Charmander: {
-		    	steps: 1,
-		    	img: "/images/charmander.jpg",
-		    	Token_info: {
-	  				AccToken: 0,
-	  				RefToken: 0
-	  			}
-		    },
-		    JigglyPuff: {
-		    	steps: 1,
-		    	img: "/images/jigglypuff.jpg",
-		    	Token_info: {
-	  				AccToken: 0,
-	  				RefToken: 0
-	  			}
-		    },
-		    Meowth: {
-		    	steps: 1,
-		    	img: "/images/meowth.jpg",
-		    	Token_info: {
-	  				AccToken: 0,
-	  				RefToken: 0
-	  			}
-		    },
-		    Pika: {
-		    	steps: 1,
-		    	img: "/images/pika.jpg",
-		    	Token_info: {
-	  				AccToken: 0,
-	  				RefToken: 0
-	  			}
-		    },
-		    Squirtle: {
-		    	steps: 1,
-		    	img: "/images/squirtle.jpg",
-		    	Token_info: {
-	  				AccToken: 0,
-	  				RefToken: 0
-	  			}
-		    }
-		}
-	};
-
-	var req = https.request({ 
-	    hostname: "glowing-heat-1885.firebaseio.com", 
-	    method: "PUT", 
-	    path: "/.json"
+	var Team1Ref = myFirebaseRef.child("Team_2");
+	var alvinRef = Team1Ref.child("Charmander");
+	alvinRef.update({
+		"steps": step
 	});
-	req.end(JSON.stringify(data));
+	// var https = require("https"),
+ //    data = {
+	//   	Team_1: {
+	// 	    Alvin: {
+	// 	    	steps: step,
+	// 	    	img: "/images/alvin.jpg",
+	// 	    	Token_info: {
+	//   				AccToken: accessToken,
+	//   				RefToken: refreshToken
+	//   			}
+	// 	    },
+	// 	    Bro: {
+	// 	    	steps: 1,
+	// 	    	img: "/images/bro.jpg",
+	// 	    	Token_info: {
+	//   				AccToken: 0,
+	//   				RefToken: 0
+	//   			}
+	// 	    },
+	// 	    Helen: {
+	// 	    	steps: 1,
+	// 	    	img: "/images/helen.jpg",
+	// 	    	Token_info: {
+	//   				AccToken: 0,
+	//   				RefToken: 0
+	//   			}
+	// 	    },
+	// 	    Paul: {
+	// 	    	steps: 1,
+	// 	    	img: "/images/paul.jpg",
+	// 	    	Token_info: {
+	//   				AccToken: 0,
+	//   				RefToken: 0
+	//   			}
+	// 	    },
+	// 	    Wes: {
+	// 	    	steps: 1,
+	// 	    	img: "/images/wes.jpg",
+	// 	    	Token_info: {
+	//   				AccToken: 0,
+	//   				RefToken: 0
+	//   			}
+	// 	    }
+	// 	},
+	// 	Team_2: {
+	// 	    Charmander: {
+	// 	    	steps: 1,
+	// 	    	img: "/images/charmander.jpg",
+	// 	    	Token_info: {
+	//   				AccToken: 0,
+	//   				RefToken: 0
+	//   			}
+	// 	    },
+	// 	    JigglyPuff: {
+	// 	    	steps: 1,
+	// 	    	img: "/images/jigglypuff.jpg",
+	// 	    	Token_info: {
+	//   				AccToken: 0,
+	//   				RefToken: 0
+	//   			}
+	// 	    },
+	// 	    Meowth: {
+	// 	    	steps: 1,
+	// 	    	img: "/images/meowth.jpg",
+	// 	    	Token_info: {
+	//   				AccToken: 0,
+	//   				RefToken: 0
+	//   			}
+	// 	    },
+	// 	    Pika: {
+	// 	    	steps: 1,
+	// 	    	img: "/images/pika.jpg",
+	// 	    	Token_info: {
+	//   				AccToken: 0,
+	//   				RefToken: 0
+	//   			}
+	// 	    },
+	// 	    Squirtle: {
+	// 	    	steps: 1,
+	// 	    	img: "/images/squirtle.jpg",
+	// 	    	Token_info: {
+	//   				AccToken: 0,
+	//   				RefToken: 0
+	//   			}
+	// 	    }
+	// 	}
+	// };
+
+	// var req = https.request({ 
+	//     hostname: "glowing-heat-1885.firebaseio.com", 
+	//     method: "PUT", 
+	//     path: "/.json"
+	// });
+	// req.end(JSON.stringify(data));
 }
 
-getStepInfo();
+start();
 
 // TODO add refresh token to db
 // TODO get refresh token from db
